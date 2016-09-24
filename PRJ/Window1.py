5033ecf37b9f61e48c1ec6e2ae79a8e5fcd9504e
LordPsyduck <andreev.alex@gmail.com>
2016-09-24 16:47:13 +0300

    Revert "Revert "Hello World""
    This reverts commit 33399901e1b7f2b924ec96bfeee4e4c6adea9869.

---
+++ PRJ/Window1.py
@@ -0,0 +1,20 @@
+#Python Config
+
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+#	w.resize(250, 150)
+#	w.move(300,300)
+	w.showMaximized()
+    w.setWindowTitle('Simple1')
+    w.show()
+
+    sys.exit(app.exec_())

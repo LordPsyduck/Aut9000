56f99e937259377b684d93ca0d5942da024a8a68
LordPsyduck <andreev.alex@gmail.com>
2016-09-24 17:56:13 +0300

    Add menu, toolbar
    No Commenatry

---
+++ Window4Stat.py
@@ -0,0 +1,38 @@
+# Python Config
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import (QMainWindow, QApplication, qApp, QAction)
+from PyQt5.QtGui import QIcon
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit Appliacation')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar().showMessage('Ready')
+
+        menubar = self.menubar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
